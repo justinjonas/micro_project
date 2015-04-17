f3303800093206b2feb0228cccdae450ff50e401
Justin Jonas <justin.r.jonas@gmail.com>
2015-04-17 11:47:15 -0600

    Added two new projects. One for temp sensor and one for register.

---
+++ temp_sensor_code/APPS_PEER2PEER2/APPS_PEER2PEER2.c
@@ -0,0 +1,24 @@
+/*
+ * APPS_PEER2PEER2.c
+ *
+ * Created: 4/6/2015 8:28:34 PM
+ *  Author: Justin Jonas
+ */ 
+
+#include "sam.h"
+
+/**
+ * \brief Application entry point.
+ *
+ * \return Unused (ANSI-C compatibility).
+ */
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    while (1) 
+    {
+        //TODO:: Please write your application code 
+    }
+}
