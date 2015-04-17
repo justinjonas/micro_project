05a9782d08808a381c6aabcf47fa38a5f3d25d94
Justin Jonas <justin.r.jonas@gmail.com>
2015-04-17 17:07:17 -0600

    more stuff

--- Kozy_Main/Kozy_Main/src/main.c
+++ Kozy_Main/Kozy_Main/src/main.c
@@ -1,8 +1,18 @@
 #include <asf.h>
-#include "wireless.h"
 #include "tasks.h"
 #include "sys.h"
 #include "conf_sio2host.h"
+#include "config.h"
+#include "nwk.h"
+#include "phy.h"
+
+typedef struct wireless_packet
+{
+	uint8_t* data;
+	uint8_t size;
+	uint8_t dst_addr;
+	uint8_t sender_addr;
+};
 
 uint8_t* TEMP_QUEUE;
 uint8_t* REGISTER_QUEUE;
@@ -33,7 +43,10 @@ static void test_task(void *params);
 //functions
 static void configure_console(void);
 void updateDisplay();
-
+void wireless_init(void);
+void send_packet(struct wireless_packet packet);	//Sends data based on the struct passed in with packet
+bool receive_packet(NWK_DataInd_t *ind);			//Callback function when a packet is received
+void send_packet_conf(NWK_DataReq_t *req);			//Callback function for a confirmed sent packet
 
 int main (void)
 {
@@ -127,4 +140,65 @@ void updateDisplay()
 	putchar(128);
 	//update display
 	printf("Mode:%s  Rm:%2dTarget:%2d%c %2d%c %s", mode, roomSelection, targetTemp, degree, roomTemp, degree, ventStatus);
+}
+
+void wireless_init(void)
+{
+	NWK_SetAddr(APP_ADDR);
+	NWK_SetPanId(APP_PANID);
+	PHY_SetChannel(APP_CHANNEL);
+	#ifdef PHY_AT86RF212
+	PHY_SetBand(APP_BAND);
+	PHY_SetModulation(APP_MODULATION);
+	#endif
+	PHY_SetRxState(true);
+	PHY_SetTxPower(0x23);
+	NWK_SetSecurityKey((uint8_t *)APP_SECURITY_KEY);
+	NWK_OpenEndpoint(APP_ENDPOINT, receive_packet);
+}
+
+//We will need to sent the struct of the payload and know where to send it to
+// TODO: Change the endpoint, destination addr, and the data payload to send
+void send_packet(struct wireless_packet packet)
+{
+	NWK_DataReq_t appDataReq;
+
+	appDataReq.dstAddr = packet.dst_addr;
+	appDataReq.dstEndpoint = packet.dst_addr;
+	appDataReq.srcEndpoint = APP_ENDPOINT;
+	appDataReq.options = NWK_OPT_ENABLE_SECURITY;
+	appDataReq.data = packet.data;
+	appDataReq.size = packet.size;
+	appDataReq.confirm = send_packet_conf;
+	NWK_DataReq(&appDataReq);
+
+}
+
+
+//When a packet is received, parse the data into the correct queues
+bool receive_packet(NWK_DataInd_t *ind)
+{	
+	switch(ind->srcAddr)
+	{
+		case TEMP_ADDR:
+			memcpy(ind->data,TEMP_QUEUE, ind->size);
+			break;
+		case REGISTER_ADDR:
+			memcpy(ind->data,REGISTER_QUEUE, ind->size);
+			break;
+		default:
+			return false;
+			//break;
+			//Call a function to add a new temp sensor and register to the network		
+	}
+	return true;
+}
+
+
+void send_packet_conf(NWK_DataReq_t *req)
+{
+	if (NWK_SUCCESS_STATUS == req->status){
+		LED_Toggle(LED0);
+		//release the semaphore ;
+	}
 }
